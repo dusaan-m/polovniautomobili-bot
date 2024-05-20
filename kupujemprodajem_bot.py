8a8430dccc11821785965a01551dfd1b144c0d2b
Dušan Milivojević <dusanmilivojevic01@gmail.com>
2024-05-21 01:48:09 +0200

    integrate new database, add stop and resume command

--- kupujemprodajem_bot.py
+++ kupujemprodajem_bot.py
@@ -1,28 +1,59 @@
 import os
 
 from dotenv import load_dotenv
+from loguru import logger
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
-from loguru import logger
 
 import scheduler
+from database import user_db, User
 
 load_dotenv()
 API_TOKEN = os.getenv("BOT_API_TOKEN")
 
 
 async def send_welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.info(f"Start command recieved from {update.message.chat.id}")
     await update.message.reply_text("Posalji mi link ka pretrazi kupujemprodajem")
 
 
 async def scrape_kp(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.message.chat.id
     url = update.message.text
-    scheduler.add_job(app.bot, update.message.chat.id, url)
     logger.info(f"Message recieved: {url}")
+
+    if not user_db.get_user(user_id):
+        user = User(user_id=user_id, url=url, run=True, cars=[])
+        user_db.add_user(user)
+
+    scheduler.add_job(app.bot, update.message.chat.id, url)
     await update.message.reply_text("Uspesno")
+
+
+async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.info(f"Stop command recieved from {update.message.chat.id}")
+    scheduler.scheduler.remove_job(str(update.message.chat.id))
+
+    user_db.update_run(update.message.chat.id, False)
+
+    logger.success(f"Job removed and stopped for user {update.message.chat.id}")
+    await update.message.reply_text("Uspesno")
+
+
+async def resume(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.info(f"Resume command recieved from {update.message.chat.id}")
+    user: User = user_db.get_user(update.message.chat.id)
+
+    if user.url:
+        scheduler.add_job(app.bot, update.message.chat.id, user_data.get("url"), resume=True)
+        await update.message.reply_text("Uspesno")
+    else:
+        await update.message.reply_text("Nemate aktivnu pretragu, posaljite link ka pretrazi kupujemprodajem")
 
 
 app = ApplicationBuilder().token(API_TOKEN).build()
 
 app.add_handler(CommandHandler('start', send_welcome))
+app.add_handler(CommandHandler('stop', stop))
+app.add_handler(CommandHandler('resume', resume))
 app.add_handler(MessageHandler(filters.Regex(r'^https://www.polovniautomobili.com/auto-oglasi/pretraga?'), scrape_kp))
