170006eb7ba97e904a7cd2b1bfd7fc4a24a3689d
Dušan Milivojević <dusanmilivojevic01@gmail.com>
2024-05-21 01:48:55 +0200

    integrate new database

--- scheduler.py
+++ scheduler.py
@@ -3,39 +3,45 @@ import json
 import os
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.util import undefined
 from loguru import logger
 
+from database import user_db, User
 from scrape_and_send_mesage import scrape_and_send_message
 
-scheduler = AsyncIOScheduler()
+scheduler = AsyncIOScheduler(job_defaults={"misfire_grace_time": 15 * 60})
 scheduler.start()
 
 
 def re_add_jobs(bot):
     logger.info("Re-adding jobs")
-    with open('users.json', 'r') as user_file:
-        user_data = json.load(user_file)
+    users: list[User] = user_db.get_all_users()
 
-    for user_id, data in user_data.items():
-        if data.get("url"):
+    for user in users:
+        user_id = str(user.user_id)
+        if user.url and user.run:
             logger.info(f"Re-adding job for user {user_id}")
-            scheduler.add_job(scrape_and_send_message, id=user_id, trigger='interval', args=[bot, user_id, data["url"]],
+            scheduler.add_job(scrape_and_send_message, id=user_id, trigger='interval', args=[bot, user_id, user.url],
                               hours=int(os.getenv("SCRAPE_INTERVAL_HOURS")),
                               next_run_time=datetime.datetime.now())
 
     logger.success("Jobs re-added")
 
 
-def add_job(bot, chat_id, url):
+def add_job(bot, chat_id, url, resume=False):
     logger.info(f"Adding job for user {chat_id}")
 
     chat_id = str(chat_id)
+    user_db.update_run(chat_id, True)
+
     if scheduler.get_job(chat_id):
         logger.info(f"Removing existing job for user {chat_id}")
         scheduler.remove_job(chat_id)
 
+    next_run_time = datetime.datetime.now() if not resume else undefined
+
     scheduler.add_job(scrape_and_send_message, id=chat_id, trigger='interval', args=[bot, chat_id, url],
                       hours=int(os.getenv("SCRAPE_INTERVAL_HOURS")),
-                      next_run_time=datetime.datetime.now())
+                      next_run_time=next_run_time)
 
     logger.success(f"Job added for user {chat_id}")
--- scrape_and_send_mesage.py
+++ scrape_and_send_mesage.py
@@ -1,49 +1,57 @@
 import asyncio
-from loguru import logger
-import json
 
+from loguru import logger
 from telegram import Bot
 
+from database import user_db, User
 from kupujemprodajem_scrape import KPScraper
 
 
-async def send_new_cars_message(bot: Bot, chat_id, url_to_cars, scraped_cars: set[str]):
+async def send_new_cars(bot: Bot, chat_id, new_cars: set[str]):
     kp_link = "https://www.polovniautomobili.com"
+
+    await bot.send_message(chat_id, f"Novi automobili:")
+
+    logger.info(f"{chat_id}: Sending {len(new_cars)} new cars to user")
+    for car in new_cars:
+        logger.success(f"{chat_id}: {car}")
+        await bot.send_message(chat_id, f"{kp_link}{car}")
+        await asyncio.sleep(1)
+
+
+async def send_no_new_cars(bot: Bot, chat_id):
+    logger.info(f"{chat_id}: No new cars")
+    await bot.send_message(chat_id, "Nema novih automobila")
+
+
+async def send_message(bot: Bot, chat_id, url_to_cars, scraped_cars: set[str]):
+    logger.info(f"Sending new cars to user {chat_id}")
     chat_id = str(chat_id)
+    user: User = user_db.get_user(chat_id)
 
-    with open('users.json', 'r') as user_file:
-        user_data = json.load(user_file)
-
-    user_data[chat_id] = user_data.get(chat_id, {})
-
-    if user_data[chat_id].get("url") != url_to_cars:
-        user_data[chat_id]["url"] = url_to_cars
-        user_data[chat_id]["cars"] = list(scraped_cars)
+    url_changed = user.url != url_to_cars
+    if url_changed:
+        user.url = url_to_cars
+        user.cars = list(scraped_cars)
+        user_db.update_url(chat_id, url_to_cars)
     else:
-        old_cars = set(user_data[chat_id]["cars"])
-        new_cars = scraped_cars - old_cars
+        old_cars = set(user.cars)
+        new_cars = scraped_cars.difference(old_cars)
 
-        if not new_cars:
-            await bot.send_message(chat_id, "Nema novih automobila")
+        if new_cars:
+            await send_new_cars(bot, chat_id, new_cars)
         else:
-            await bot.send_message(chat_id, f"Novi automobili:")
-            for car in new_cars:
-                logger.success(f"{chat_id}: {car}")
-                await bot.send_message(chat_id, f"{kp_link}{car}")
-                await asyncio.sleep(1)
+            await send_no_new_cars(bot, chat_id)
 
-        user_data[chat_id]["cars"] = list(new_cars | old_cars)
+        old_cars.update(new_cars)
+        user.cars = list(old_cars)
 
-    with open('users.json', 'w') as user_file:
-        json.dump(user_data, user_file, indent=4)
+    user_db.update_cars(chat_id, user.cars)
+
+    logger.success(f"Sending finished for user {chat_id}")
 
 
 async def scrape_and_send_message(bot: Bot, chat_id, url_to_cars):
-    logger.info(f"Scraping started url: {url_to_cars}")
-    scraper = KPScraper()
-    scraped_cars = await scraper.scrape(url_to_cars)
-    logger.success(f"Scraping finished url: {url_to_cars}")
+    scraped_cars = await KPScraper().scrape(url_to_cars)
 
-    logger.info(f"Sending new cars to user {chat_id}")
-    await send_new_cars_message(bot, chat_id, url_to_cars, scraped_cars)
-    logger.success(f"Sending finished for user {chat_id}")
+    await send_message(bot, chat_id, url_to_cars, scraped_cars)
